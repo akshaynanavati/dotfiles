38112befb663d2772a698fe58541527d755a6a7b
Akshay Nanavati <akshay.nanavati1@gmail.com>
2017-05-17 13:12:48 -0700

    Update

--- _install_sublime_conf.sh
+++ _install_sublime_conf.sh
@@ -1,3 +1,4 @@
+DOTFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 KEY_MAP_CONFIG="Default (OSX).sublime-keymap"
 PACKAGES="Package Control.sublime-settings"
 PREFERENCES="Preferences.sublime-settings"
@@ -8,6 +9,6 @@ if [ ! -d "$SUBL_DIR" ]; then
   mkdir -p "$SUBL_DIR"
 fi
 
-ln -s "sublime/$KEY_MAP_CONFIG" "$SUBL_DIR/$KEY_MAP_CONFIG"
-ln -s "sublime/$PACKAGES" "$SUBL_DIR/$PACKAGES"
-ln -s "sublime/$PREFERENCES" "$SUBL_DIR/$PREFERENCES"
+ln -s "$DOTFILE_DIR/sublime/$KEY_MAP_CONFIG" "$SUBL_DIR/$KEY_MAP_CONFIG"
+ln -s "$DOTFILE_DIR/sublime/$PACKAGES" "$SUBL_DIR/$PACKAGES"
+ln -s "$DOTFILE_DIR/sublime/$PREFERENCES" "$SUBL_DIR/$PREFERENCES"
