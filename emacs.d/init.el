3173122c30ccb3c069136d5d1e95bb86563c94d3
Akshay Nanavati <akshay.nanavati1@gmail.com>
2017-06-21 09:11:36 -0700

    Add auto complete to emacs init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,8 +13,8 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (manoj-dark)))
- '(package-selected-packages (quote (rust-mode fiplr helm use-package))))
+ '(custom-enabled-themes (quote (wombat)))
+ '(package-selected-packages (quote (auto-complete rust-mode fiplr helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -45,6 +45,9 @@
 
 (load-theme 'monokai t)
 
+(use-package auto-complete
+  :ensure t)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (setq-default tab-width 4)
@@ -62,3 +65,5 @@
 (global-linum-mode t)
 
 (setq-default c-basic-offset 4)
+
+(show-paren-mode 1)
