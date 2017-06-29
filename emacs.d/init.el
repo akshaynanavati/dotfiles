575d4c4352ff65843c5819b32ccc4a02ff1873dd
Akshay Nanavati <akshay.nanavati1@gmail.com>
2017-06-29 15:32:55 -0700

    Updates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,10 @@
+;;;
+
 (require 'package)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package management
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -14,15 +20,15 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (wombat)))
- '(package-selected-packages (quote (auto-complete rust-mode fiplr helm use-package))))
+ '(package-selected-packages
+   (quote
+    (terraform-mode auto-complete rust-mode fiplr helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(setq next-screen-context-lines 2)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -37,6 +43,8 @@
 (use-package fiplr
   :ensure t)
 
+(setq fiplr-ignored-globs '((directories (".git" ".svn" "target" ".vagrant"))
+                            (files ("*.log" "*.jpg" "*.png" "*.zip" "*~"))))
 (use-package rust-mode
   :ensure t)
 
@@ -62,10 +70,25 @@
 (use-package auto-complete
   :ensure t)
 
+(use-package terraform-mode
+  :ensure t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Editor Preferences
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
+(global-linum-mode t)
+(show-paren-mode 1)
+(setq next-screen-context-lines 2)
+(global-set-key (kbd "C-x f") 'fiplr-find-file)
+(global-set-key (kbd "C-x t") 'neotree-toggle)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Language Specific Hooks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-hook 'emacs-lisp-mode-hook
   (lambda ()
@@ -75,18 +98,17 @@
           (lambda ()
             (set-key (kbd "C-x C-k") 'compile)))
 
-(add-hook 'cider-repl-mode-hook
-      '(lambda () (define-key cider-repl-mode-map (kbd "C-x c")
-            'cider-repl-clear-buffer)))
-
-(global-set-key (kbd "C-x f") 'fiplr-find-file)
-(global-set-key (kbd "C-x t") 'neotree-toggle)
-
-(setq fiplr-ignored-globs '((directories (".git" ".svn" "target" ".vagrant"))
-                            (files ("*.log" "*.jpg" "*.png" "*.zip" "*~"))))
-
-(global-linum-mode t)
-
 (setq-default c-basic-offset 4)
 
-(show-paren-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clojure
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-hook 'cider-repl-mode-hook
+      '(lambda () (define-key cider-repl-mode-map (kbd "C-x c")
+                    'cider-repl-clear-buffer)))
+
+(add-hook 'cider-repl-mode-hook #'eldoc-mode)
+
+(provide 'init)
+;;;
